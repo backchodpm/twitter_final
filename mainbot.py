c306e20051f7c0c76bac0c9bcad81f172cf98b14
backchodpm <61050881+backchodpm@users.noreply.github.com>
2020-09-24 17:36:59 +0530

    Update mainbot.py

--- mainbot.py
+++ mainbot.py
@@ -30,9 +30,9 @@ def follower_count(user):
 api = create_api()
 
 while True:
-    user = api.get_user('the_ameen_manna')
-    api.update_profile(name=f'AMEEN|{follower_count(user)} Followers')
-    print(f'Updating Twitter Name : AMEEN|{follower_count(user)} Followers')
+    user = api.get_user('ProkorshoM')
+    api.update_profile(name=f'Prokorsho Munshi|{follower_count(user)} Followers')
+    print(f'Updating Twitter Name : Prokorsho Munshi|{follower_count(user)} Followers')
     print('Waiting to refresh')
     time.sleep(60)
   
